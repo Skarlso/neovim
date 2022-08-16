14f3543992c57180c5fb7c0c97b151091a6bf94a
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-08-16 10:39:34 +0200

    Added some stuff

--- lua/settings.lua
+++ lua/settings.lua
@@ -7,7 +7,7 @@ vim.opt.autoindent = true
 vim.opt.backspace = {'indent','eol','start'}
 vim.opt.complete:remove('i')
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 0
 vim.opt.expandtab = true
